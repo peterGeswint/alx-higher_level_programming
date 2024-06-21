794b46b118d1c03b9c8b24abc512ed8159525393
peterGeswint <petergeswint@outlook.com>
2024-06-21 06:59:10 -0700

    task1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,11 +2,12 @@
 
 'use strict';
 
-/* eslint-disable no-unused-vars */
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
-/* eslint-enable no-unused-vars */
+
+// Export the Rectangle class
+module.exports = Rectangle;
