91648086cfc89c5b0928dd3ae0379c773954019a
peterGeswint <petergeswint@outlook.com>
2024-06-21 06:15:01 -0700

    task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,19 +4,16 @@ function secondBiggest (args) {
   if (args.length <= 1) {
     return 0;
   }
-  
   let max = -Infinity;
   let secondMax = -Infinity;
-  
   for (const num of args) {
-     if (num > max) {
+    if (num > max) {
       secondMax = max;
       max = num;
     } else if (num > secondMax && num < max) {
       secondMax = num;
     }
   }
-  
   return secondMax;
 }
 
