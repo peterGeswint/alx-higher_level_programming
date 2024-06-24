9e458206832df6c14bd94fc6f4ce50eb5e145fc0
peterGeswint <petergeswint@outlook.com>
2024-06-24 09:53:10 -0700

    task 100

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -5,7 +5,8 @@ const myObject = {
   value: 12
 };
 
-// Modify myVar to 333
+// Update the value
 myObject.value = 333;
 
-console.log(myObject); // Output the modified object to verify
+// Print the updated value
+console.log(myObject.value);
