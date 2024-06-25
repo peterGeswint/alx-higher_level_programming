b9b8c596f36ccb12d0f9c5592b58ab79a6086e77
peterGeswint <petergeswint@outlook.com>
2024-06-25 02:03:55 -0700

    task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
+}
+
+console.log(newDict);
