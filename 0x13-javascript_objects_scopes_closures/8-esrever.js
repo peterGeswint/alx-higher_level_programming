8e327c4bc5e9d944e630b12e70ce475e2dd0850b
peterGeswint <petergeswint@outlook.com>
2024-06-25 01:19:27 -0700

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let len = list.lenght - 1;
+  let len = list.length - 1;
   let i = 0;
-  while ((len - 1) > 0) {
+  while (i < len) {
     const aux = list[len];
     list[len] = list[i];
     list[i] = aux;
