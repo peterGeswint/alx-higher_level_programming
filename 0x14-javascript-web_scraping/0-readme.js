956a019532705308c37287e768ee29bee6dfd053
peterGeswint <petergeswint@outlook.com>
2024-08-18 12:39:12 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 function readFile (filePath) {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.error('Error: ${err.message}');
+      console.error(`Error: ${err.message}, ${err.message}`);
       return;
     }
     console.log(data);
