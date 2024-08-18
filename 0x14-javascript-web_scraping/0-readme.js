6cfb05707dfa3bf8f1153291fd6a5eed1d9baee4
peterGeswint <petergeswint@outlook.com>
2024-08-18 12:19:39 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 /* eslint-disable no-undef */
-
 const fs = require('fs');
 
-function readFile(filePath) {
-	  fs.readFile(filePath, 'utf8', (err, data) => {
-		      if (err) {
-			            console.error(`An error occurred: ${err.message}`);
-			            return;
-			          }
-		      console.log(data);
-		    });
+function readFile (filePath) {
+	fs.readFile(filePath, 'utf8', (err, data) => {
+		if (err) {
+			console.error('Error: ${err.message)');
+			return;
+		}
+		console.log(data);
+	});
 }
 
 if (process.argv.length < 3) {
-	  console.log('Please provide the file path as the first argument.');
+	console.log('Please provide the file path as the first argument.');
 } else {
-	  readFile(process.argv[2]);
+	readFile(process.argv[2]);
 }
-
